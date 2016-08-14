ff9f7bcc31d833ed48bd05a84a202c7b61237453
Dave Harned <dharned@gmail.com>
2016-08-14 13:42:10 -0500

    problem with promise

--- tests/validate-test.js
+++ tests/validate-test.js
@@ -6,27 +6,32 @@ var expect = require('chai').expect;
 var validator = require('../validate.js');
 
 
-describe('validator() function', function() {
+describe('validator object', function() {
 	// Test 1 - basic setup
 	it('should exist', function() {
 		expect(validator).to.not.be.undefined;
 	});
 
 	// Test 2 - detect illegal character
-	it('should accept URL, and return an error if it contains illegal characters', function() {
-		var expErr = "Error, an illegal character was detected";
-		expect(validator.tester('http://www.go|ogle.com/')).to.eql(expErr);
+	it('should accept URL, and return a message if it contains illegal characters', function() {
+		var expErr = "URL contains an illegal character";
+		expect(validator.formatTest('http://www.go|ogle.com/')).to.eql(expErr);
 	})
 
 	// Test 3 - detect missing protocol
-	it('should accept URL, and return an error if protocol is missing', function() {
-		var expErr = "Error, your URL is missing a protocol like 'http'";
-		expect(validator.tester('www.google.com/lolwut')).to.eql(expErr);
+	it('should accept URL, and return a message if protocol is missing', function() {
+		var expErr = "URL is missing a protocol like 'http'";
+		expect(validator.formatTest('www.google.com/lolwut')).to.eql(expErr);
 	});
 
 	// Test 4 - detect missing dot in hostname
-	it('should accept URL, and return an error if hostname has no dot', function() {
-		var expErr = "your URL hostname doesn't look quite right";
-		expect(validator.tester("http://ihavenoideawhatimdoing/")).to.eql(expErr);
+	it('should accept URL, and return a message if hostname has no dot', function() {
+		var expErr = "URL hostname not formatted correctly";
+		expect(validator.formatTest('http://ihavenoideawhatimdoing/')).to.eql(expErr);
 	});
+
+	//Test 5 - test url network call
+	it('should accept URL, and return promise object', function() {
+		expect(validator.networkTest('http://www.google.com')).then.to.be.an('object');
+	})
 });
--- validate.js
+++ validate.js
@@ -3,29 +3,47 @@ var http = require('http');
 
 var validator = {
 
-	tester: function(testUrl) {
+	formatTest: function(testUrl) {
 
 		// check URL for illegal characters, return error
 		var illegalChar = new RegExp('["<>\\^`{}|]');
 		var illegal = illegalChar.test(testUrl);
-		if(illegal) { return "Error, an illegal character was detected" };
+		if(illegal) { return "URL contains an illegal character" };
 
 		// check for url protocol
 		var urlObj = url.parse(testUrl);
-		if(urlObj.protocol == undefined) { return "Error, your URL is missing a protocol like 'http'" };
+		if(urlObj.protocol == undefined) { return "URL is missing a protocol like 'http'" };
 
 		// check if hostname contains '.'
 		var dot = new RegExp('.');
-		var dotMissing = dot.test(urlObj.hostname);
-		if(dotMissing) { return "your URL hostname doesn't look quite right"};
+		var dotFound = dot.test(urlObj.hostname);
+		if(!dotFound) { return "URL hostname not formatted correctly"};
 
 		// make an http request
+		// this.networkTest(testUrl)
+		// 	.then(function(response){
+		// 		console.log(response);
+		// 	})
+		// 	.catch(function(err){
+		// 		console.log(error);
+		// 	})
+	},
 
-		// check if status code starts with 2
+	networkTest: function(testUrl) {
+		// return pending promise
+		return new Promise(function(resolve){
 
+			var request = http.get(testUrl, function(response){
+				if (response.statusCode < 200 || response.statusCode > 299) {
+					resolve(new Error('Failed to load page, status code: ' + response.statusCode))
+				} else {
+					resolve('Success');
+				};
+			});
+		});
 	}
 };
 
-// console.log(validator.tester('What in the world is this'));
+console.log(validator.formatTest('http://w2.ww.goo.asdgle.com/'));
 
 module.exports = validator;
