a8b37465b96b8e5f05baf731c5ecf2073b39f676
Dave Harned <dharned@gmail.com>
2016-08-14 13:04:55 -0500

    built out url tester function

--- tests/validate-test.js
+++ tests/validate-test.js
@@ -12,7 +12,21 @@ describe('validator() function', function() {
 		expect(validator).to.not.be.undefined;
 	});
 
-	it('should accept URL string, and return an error if it contains illegal characters', function() {
-		expect(validator.tester('http://www.google.com/'))
+	// Test 2 - detect illegal character
+	it('should accept URL, and return an error if it contains illegal characters', function() {
+		var expErr = "Error, an illegal character was detected";
+		expect(validator.tester('http://www.go|ogle.com/')).to.eql(expErr);
 	})
+
+	// Test 3 - detect missing protocol
+	it('should accept URL, and return an error if protocol is missing', function() {
+		var expErr = "Error, your URL is missing a protocol like 'http'";
+		expect(validator.tester('www.google.com/lolwut')).to.eql(expErr);
+	});
+
+	// Test 4 - detect missing dot in hostname
+	it('should accept URL, and return an error if hostname has no dot', function() {
+		var expErr = "your URL hostname doesn't look quite right";
+		expect(validator.tester("http://ihavenoideawhatimdoing/")).to.eql(expErr);
+	});
 });
--- validate.js
+++ validate.js
@@ -1,12 +1,23 @@
+var url = require('url');
+var http = require('http');
+
 var validator = {
 
-	tester: function(url) {
+	tester: function(testUrl) {
 
-		// check URL for illegal characters
+		// check URL for illegal characters, return error
+		var illegalChar = new RegExp('["<>\\^`{}|]');
+		var illegal = illegalChar.test(testUrl);
+		if(illegal) { return "Error, an illegal character was detected" };
 
-		// check for protocol
+		// check for url protocol
+		var urlObj = url.parse(testUrl);
+		if(urlObj.protocol == undefined) { return "Error, your URL is missing a protocol like 'http'" };
 
 		// check if hostname contains '.'
+		var dot = new RegExp('.');
+		var dotMissing = dot.test(urlObj.hostname);
+		if(dotMissing) { return "your URL hostname doesn't look quite right"};
 
 		// make an http request
 
@@ -14,5 +25,7 @@ var validator = {
 
 	}
 };
+
+// console.log(validator.tester('What in the world is this'));
 
 module.exports = validator;
