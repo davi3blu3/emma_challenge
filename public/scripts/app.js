f2beaf4ba66704ad47f5fb76de940068a2f0d678
Dave Harned <dharned@gmail.com>
2016-08-14 14:02:33 -0500

    start challenge b, visual draft and react hooked up

---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+// var Test = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div>
+// 				<h1>HELLOWORLD</h1>
+// 			</div>
+// 		);
+// 	}
+// });
+
+// ReactDOM.render(
+// 	<Test />,
+// 	document.getElementById('content')
+// );
