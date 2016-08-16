70df8b30ad1b46be5d770b4f9cdd9312660bdd62
Dave Harned <dharned@gmail.com>
2016-08-16 00:34:59 -0500

    link lookup app working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,6 @@ var Application = React.createClass({
 			<div className="container">
 			  <h1 className="text-center title">Link Lookup</h1>
 			  <InputForm />
-			  <br/>
-			  <LinkOutput />
 			</div>
 		);
 	}
@@ -14,7 +12,8 @@ var Application = React.createClass({
 var InputForm = React.createClass({
 	getInitialState: function() {
 		return {
-			text: ""
+			text: "",
+			links: []
 		}
 	},
 	handleChange: function(event) {
@@ -34,32 +33,33 @@ var InputForm = React.createClass({
             type: 'GET',
             contentType: 'application/json',
             data: urlInput,
-            success: function(data) {
-            	console.log(data);
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-	            console.log(xhr.status);
-	            console.log(thrownError);
-            }
+            success: function(rawHtml) {
+            	// regex to look for links in html
+            	var hrefs = /(href=".*?)"/g;
+            	var links = rawHtml.match(hrefs);
+            	this.setState({links: links});
+            }.bind(this),
+            error: function (xhr, status, error) {
+	            console.error(this.props.url, status, err.toString());
+            }.bind(this)
         });
-	},
-	processLinks: function(rawHtml) {
-			console.log('processlinks triggered!');
-			var hrefs = /(href=".*?)"/g;
-			var links = rawHtml.match(hrefs);
 	},
 	render: function() {
 		return (
-			<form className="well url-entry">
-	    		<label htmlFor="url">Please enter a URL:</label>
-	    		<div className="input-group">
-	    			<span className="input-group-addon" id="basic-addon3">http://</span>
-	      			<input type="text" className="form-control" id="basic-url" value={this.state.text} onChange={this.handleChange}/>
-	      			<span className="input-group-btn">
-	        			<button className="btn btn-primary" type="button" disabled={this.state.text.length === 0} onClick={this.handleSubmit}>Submit</button>
-	      			</span>
-	    		</div>
-	  		</form>
+			<div>
+				<form className="well url-entry">
+		    		<label htmlFor="url">Please enter a URL:</label>
+		    		<div className="input-group">
+		    			<span className="input-group-addon" id="basic-addon3">http://</span>
+		      			<input type="text" className="form-control" id="basic-url" value={this.state.text} onChange={this.handleChange}/>
+		      			<span className="input-group-btn">
+		        			<button className="btn btn-primary" type="button" disabled={this.state.text.length === 0} onClick={this.handleSubmit}>Submit</button>
+		      			</span>
+		    		</div>
+		  		</form>
+		  		<br/>
+				<LinkOutput data={this.state.links} />
+	  		</div>
 		);
 	}
 });
@@ -68,27 +68,31 @@ var LinkOutput = React.createClass({
 	getInitialState: function() {
 		return {
 			// links to be iterated in return statement, if present
-			links: ["/react/favicon.ico", "https://facebook.github.io/react/feed.xml", "https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.csso", "/react/css/syntax.css"]
+			// links: ["/react/favicon.ico", "https://facebook.github.io/react/feed.xml", "https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.csso", "/react/css/syntax.css"]
 			// links: []
 		};
 	},
 	render: function() {
-		var linksList = this.state.links.map(function(link, index){
-			return <li className="list-group-item" key={index}>{link}</li>;
-		})
-		if (this.state.links.length > 0) {
-			// section only renders when 'links' data is present
-			return (
-				<div className="well">
-				<h3 className="text-center">Links contained in {URL} markup</h3>
-				<ul className="list-group">
-					{ linksList }
-				</ul>
-				</div>
-			);
-		} else {
-			return null;
-		}
+			if (this.props.data.length == 0) {
+				return null
+			} else {
+				var linksList = this.props.data.map(function(link, index){
+					return <li className="list-group-item" key={index}>{link}</li>;
+				});
+
+				// section only renders when 'links' data is present
+				return (
+					<div className="well">
+						<h3 className="text-center">Links contained in {URL} markup</h3>
+						<ul className="list-group">
+							{ linksList }
+						</ul>
+					</div>
+				);
+			};
+			
+
+
 	}
 });
 
